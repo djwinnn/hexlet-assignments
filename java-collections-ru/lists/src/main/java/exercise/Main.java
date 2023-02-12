cb241ba7b32d5aee1388632cbf46e254f10863e1
@hexlet/cli <support@hexlet.io>
2023-02-13 00:30:24 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/Main.java
+++ java-collections-ru/lists/src/main/java/exercise/Main.java
@@ -2,7 +2,7 @@ package exercise;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(App.scrabble("begsdhhtsexoult", "Hexlet"));
+        System.out.println(App.scrabble("", "java"));
 
     }
 }
