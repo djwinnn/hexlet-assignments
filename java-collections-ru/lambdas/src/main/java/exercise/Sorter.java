afcadb778cfdc163bbeef1f3e9f812026af5abe2
@hexlet/cli <support@hexlet.io>
2023-05-27 19:31:50 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,8 +10,12 @@ public class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> list) {
         return list.stream()
                 .filter(pet -> pet.get("gender").equals("male"))
+                .sorted((u1, u2) -> {
+                    LocalDate b1 = LocalDate.parse(u1.get("birthday"));
+                    LocalDate b2 = LocalDate.parse(u2.get("birthday"));
+                    return b1.compareTo(b2); // сортируем по возрасту
+                })
                 .map(pet -> pet.get("name"))
-                .sorted(String::compareTo)
                 .collect(Collectors.toList());
     }
 }
