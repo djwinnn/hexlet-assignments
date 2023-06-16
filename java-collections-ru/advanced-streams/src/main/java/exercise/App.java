3aecdb74e21f3c34d58615532cb87f96282822db
@hexlet/cli <support@hexlet.io>
2023-06-16 19:38:56 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,30 +1,26 @@
 package exercise;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
 // BEGIN
 public class App {
-    public static String getForwardedVariables(String config) {
-        StringBuilder result = new StringBuilder();
-        Pattern pattern = Pattern.compile("X_FORWARDED_\\w+=");
-
-        String[] parts = config.split("(environment=|,)");
-        for (String part : parts) {
-            Matcher matcher = pattern.matcher(part);
-            if (matcher.find()) {
-                String varName = matcher.group().substring(13, matcher.group().length() - 1);
-                String varValue = part.substring(matcher.end(), part.length());
-                result.append(varName).append("=").append(varValue).append(",");
-            }
-        }
-
-        if (result.length() > 0) {
-            result.deleteCharAt(result.length() - 1);
-        }
-        return result.toString();
+    public static String getForwardedVariables(String configFileContent) {
+        Map<String, String> environmentVariables = new HashMap<>();
+        Arrays.stream(configFileContent.split("\n"))
+                .filter(line -> line.startsWith("environment="))
+                .map(line -> line.replace("environment=\"", "").replace("\"", ""))
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .filter(pair -> pair.startsWith("X_FORWARDED_"))
+                .forEach(pair -> {
+                    String[] keyValue = pair.split("=");
+                    environmentVariables.put(keyValue[0].replace("X_FORWARDED_", ""), keyValue[1]);
+                });
+        return environmentVariables.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .collect(Collectors.joining(","));
     }
 }
 //END
