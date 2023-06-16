7c0030a234152196eca038e7d8b5622e299a0eee
@hexlet/cli <support@hexlet.io>
2023-06-16 19:30:47 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,34 +1,30 @@
 package exercise;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
 // BEGIN
 public class App {
-    public static String getForwardedVariables(String content) {
-        Map<String, String> result = new HashMap<>();
-        String[] lines = content.split("\n");
-        for (String line : lines) {
-            if (line.contains("environment")) {
-                String[] split1 = line.split("\"");
-                String[] env_vars = split1[1].split(",");
-                for (String env_var : env_vars) {
-                    if (env_var.startsWith("X_FORWARDED_")) {
-                        String[] env_var_split = env_var.split("=");
-                        String key = env_var_split[0].replace("X_FORWARDED_", "");
-                        String value = env_var_split[1];
-                        result.put(key, value);
-                    }
-                }
+    public static String getForwardedVariables(String config) {
+        StringBuilder result = new StringBuilder();
+        Pattern pattern = Pattern.compile("X_FORWARDED_\\w+=");
+
+        String[] parts = config.split("(environment=|,)");
+        for (String part : parts) {
+            Matcher matcher = pattern.matcher(part);
+            if (matcher.find()) {
+                String varName = matcher.group().substring(13, matcher.group().length() - 1);
+                String varValue = part.substring(matcher.end(), part.length());
+                result.append(varName).append("=").append(varValue).append(",");
             }
         }
-        StringBuilder builder = new StringBuilder();
-        for (Map.Entry<String, String> entry : result.entrySet()) {
-            builder.append(entry.getKey()).append("=").append(entry.getValue()).append(",");
+
+        if (result.length() > 0) {
+            result.deleteCharAt(result.length() - 1);
         }
-        builder.setLength(builder.length() - 1);
-        return builder.toString();
+        return result.toString();
     }
 }
+//END
